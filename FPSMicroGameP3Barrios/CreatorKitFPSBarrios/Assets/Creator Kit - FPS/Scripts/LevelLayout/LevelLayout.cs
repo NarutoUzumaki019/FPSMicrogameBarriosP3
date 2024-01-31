0bf0fccc004b3f1539adedee4fc275cf4074f0f7
NarutoUzumaki019 <145381119+NarutoUzumaki019@users.noreply.github.com>
2024-01-31 11:34:32 -0800

    I did stuff
    I did stuff

--- FPSMicroGameP3Barrios/CreatorKitFPSBarrios/Assets/Creator Kit - FPS/Scripts/LevelLayout/LevelLayout.cs
+++ FPSMicroGameP3Barrios/CreatorKitFPSBarrios/Assets/Creator Kit - FPS/Scripts/LevelLayout/LevelLayout.cs
@@ -29,7 +29,7 @@ public class LevelLayout : MonoBehaviour
         
         foreach (var room in rooms)
         {
-            room.gameObject.hideFlags = HideFlags.HideInHierarchy;
+            room.gameObject.hideFlags = HideFlags.HideInHierarchy; 
         }
     }
 #endif
